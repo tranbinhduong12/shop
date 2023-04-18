85482ccb967a65601249811d3230f59d2284e2b8
dwg1207 <tranduong12072001@gmail.com>
2023-04-19 01:59:26 +0700

    add middleware page admin, add view page admin

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\MainController;
+use App\Http\Controllers\Admin\Users\LoginController;
+use App\Providers\RouteServiceProvider;
+
+Route::get('admin/users/login', [LoginController::class, 'index'])->name('login');
+Route::post('admin/users/login/store', [LoginController::class, 'store']);
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('admin', [MainController::class, 'index'])->name('admin');
+    Route::get('admin/main', [MainController::class, 'index']);
+});
