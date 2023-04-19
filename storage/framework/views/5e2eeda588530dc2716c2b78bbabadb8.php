1bb21667c20f48fe40e85aa75ff1412d8f30953d
dwg1207 <tranduong12072001@gmail.com>
2023-04-19 10:58:52 +0700

    dang nhap admin

---
+++ storage/framework/views/5e2eeda588530dc2716c2b78bbabadb8.php
@@ -0,0 +1,6 @@
+<?php $__env->startSection('content'); ?>
+Nội dung
+<?php $__env->stopSection(); ?>
+
+
+<?php echo $__env->make('admin.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\shop\resources\views/admin/home.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/8379ecd9066c443360925827987a81c8.php
@@ -0,0 +1,25 @@
+<?php if($errors->any()): ?>
+    <div class="alert alert-danger">
+        <ul>
+            <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <li><?php echo e($error); ?></li>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </ul>
+    </div>
+<?php endif; ?>
+
+
+<?php if(Session::has('error')): ?>
+    <div class="alert alert-danger">
+        <?php echo e(Session::get('error')); ?>
+
+    </div>
+<?php endif; ?>
+
+<?php if(Session::has('success')): ?>
+    <div class="alert alert-success">
+        <?php echo e(Session::get('success')); ?>
+
+    </div>
+<?php endif; ?>
+<?php /**PATH C:\laragon\www\shop\resources\views/admin/alert.blade.php ENDPATH**/ ?>
